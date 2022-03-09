46a1719ec6ab805e72d645266d1d0ff689ccbb0d
Nessvah <vanessamarquesns@outlook.pt>
2022-03-09 11:23:53 +0000

    add funcionality to remove scroll on toggle menu

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,14 +1,19 @@
 const hamburguer = document.querySelector(".hamburguer");
 const navMenu = document.querySelector(".nav-menu");
+const hamburguerActive = document.querySelector(".hamburguer.active");
 
 hamburguer.addEventListener("click", () => {
   hamburguer.classList.toggle("active");
   navMenu.classList.toggle("active");
+  document.body.classList.add("noscroll");
 });
 
 document.querySelectorAll(".nav-link").forEach((n) =>
   n.addEventListener("click", () => {
     hamburguer.classList.remove("active");
     navMenu.classList.remove("active");
+    document.body.classList.remove("noscroll");
   })
 );
+
+//TODO: find a way to enable scroll if user clicks on active hamburguer instead of nav-links
